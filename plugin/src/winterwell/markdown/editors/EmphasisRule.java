0f90f57e6bc1e14e16ae0db70383bed75600c156
Amir Pakdel <pakdel@gmail.com>
2013-01-26 00:12:09 -0500

    Added list and link rules

--- plugin/src/winterwell/markdown/editors/EmphasisRule.java
+++ plugin/src/winterwell/markdown/editors/EmphasisRule.java
@@ -5,39 +5,108 @@
  */
 package winterwell.markdown.editors;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.text.rules.ICharacterScanner;
+import org.eclipse.jface.text.rules.IRule;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.MultiLineRule;
+import org.eclipse.jface.text.rules.Token;
 
 /**
  * 
  *
  * @author Daniel Winterstein
  */
-public class EmphasisRule extends MultiLineRule {
+public class EmphasisRule implements IRule {
+	private static char[][] fDelimiters = null;
+	private char[] fSequence;
+	protected IToken fToken;
 
 
 	public EmphasisRule(String marker, IToken token) {
-		super(marker, marker, token);
 		assert marker.equals("*") || marker.equals("_") || marker.equals("**")
 				|| marker.equals("***");
+		Assert.isNotNull(token);
+		fSequence = marker.toCharArray();
+		fToken = token;
 	}
 	
-	@Override
+	// Copied from org.eclipse.jface.text.rules.PatternRule
 	protected boolean sequenceDetected(ICharacterScanner scanner, char[] sequence, boolean eofAllowed) {
-		boolean detect = super.sequenceDetected(scanner, sequence, eofAllowed);
-		if (!detect) return false;
-		// But is it an emphasis* or a * list item?
-		for(int i=0; i <= sequence.length; i++) scanner.unread();
-		char before = (char) scanner.read();
-		String star = "";
-		for(int i=0; i <sequence.length; i++) star += scanner.read();
-		char after = (char) scanner.read();
-		// Set the scanner back to where it was
-		scanner.unread();
-		// Is it a list item marker, or just a floating *?
-		if (Character.isWhitespace(after) && Character.isWhitespace(before)) return false;
+		for (int i = 1; i < sequence.length; i++) {
+			int c = scanner.read();
+			if (c == ICharacterScanner.EOF && eofAllowed) {
+				return true;
+			} else if (c != sequence[i]) {
+				// Non-matching character detected, rewind the scanner back to
+				// the start.
+				// Do not unread the first character.
+				for (int j = i; j > 0; j--)
+					scanner.unread();
+				return false;
+			}
+		}
 		return true;
+	}
+
+	/*
+	 * @see IRule#evaluate(ICharacterScanner)
+	 * 
+	 * @since 2.0
+	 */
+	public IToken evaluate(ICharacterScanner scanner) {
+		// Should be connected only on the right side
+		scanner.unread();
+		boolean sawSpaceBefore = Character.isWhitespace(scanner.read());
+		if (!sawSpaceBefore && scanner.getColumn() != 0) {
+			return Token.UNDEFINED;
+		}
+
+		int c = scanner.read();
+		// Should be connected only on right side
+		if (c != fSequence[0] || !sequenceDetected(scanner, fSequence, false)) {
+			scanner.unread();
+			return Token.UNDEFINED;
+		}
+		int readCount = fSequence.length;
+		if (fDelimiters == null) {
+			fDelimiters = scanner.getLegalLineDelimiters();
+		}
+		// Start sequence detected
+		int delimiterFound = 0;
+		// Is it a list item marker, or just a floating *?
+		if (sawSpaceBefore) {
+			boolean after = Character.isWhitespace(scanner.read());
+			scanner.unread();
+			if (after)
+				delimiterFound = 2;
+		}
+
+		while (delimiterFound < 2
+				&& (c = scanner.read()) != ICharacterScanner.EOF) {
+			readCount++;
+
+			if (!sawSpaceBefore && c == fSequence[0]
+					&& sequenceDetected(scanner, fSequence, false)) {
+				return fToken;
+			}
+
+			int i;
+			for (i = 0; i < fDelimiters.length; i++) {
+				if (c == fDelimiters[i][0]
+						&& sequenceDetected(scanner, fDelimiters[i], true)) {
+					delimiterFound++;
+					break;
+				}
+			}
+			if (i == fDelimiters.length)
+				delimiterFound = 0;
+			sawSpaceBefore = Character.isWhitespace(c);
+		}
+		// Reached ICharacterScanner.EOF
+		for (; readCount > 0; readCount--)
+			scanner.unread();
+		return Token.UNDEFINED;
 	}
 	
 }
---
+++ plugin/src/winterwell/markdown/editors/LinkRule.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright winterwell Mathematics Ltd.
+ * @author Daniel Winterstein
+ * 11 Jan 2007
+ */
+package winterwell.markdown.editors;
+
+import org.eclipse.jface.text.rules.ICharacterScanner;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.Token;
+import org.eclipse.jface.text.rules.IRule;
+import org.eclipse.core.runtime.Assert;
+
+/**
+ * 
+ *
+ * @author Amir Pakdel
+ */
+public class LinkRule implements IRule {
+	private static char[][] fDelimiters = null;
+	protected IToken fToken;
+
+	public LinkRule(IToken token) {
+		Assert.isNotNull(token);
+		fToken= token;
+	}
+	
+	/*
+	 * @see IPredicateRule#getSuccessToken()
+	 * @since 2.0
+	 */
+	public IToken getSuccessToken() {
+		return fToken;
+	}
+
+
+	// Copied from org.eclipse.jface.text.rules.PatternRule
+	protected boolean sequenceDetected(ICharacterScanner scanner, char[] sequence, boolean eofAllowed) {
+		for (int i= 1; i < sequence.length; i++) {
+			int c= scanner.read();
+			if (c == ICharacterScanner.EOF && eofAllowed) {
+				return true;
+			} else if (c != sequence[i]) {
+				// Non-matching character detected, rewind the scanner back to the start.
+				// Do not unread the first character.
+				scanner.unread();
+				for (int j= i-1; j > 0; j--)
+					scanner.unread();
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * @see IRule#evaluate(ICharacterScanner)
+	 * @since 2.0
+	 */
+	public IToken evaluate(ICharacterScanner scanner) {
+		int c;
+		if ((c = scanner.read()) != '[') {
+			if ((c != 'h' || ( !sequenceDetected(scanner, "http://".toCharArray(), false) && !sequenceDetected(scanner, "https://".toCharArray(), false) ))
+					&& (c != 'f' || !sequenceDetected(scanner, "ftp://".toCharArray(), false)) ) {
+				// Not even a non-standard link
+				scanner.unread();
+				return Token.UNDEFINED;
+			}
+			
+			// Non-standard link
+			while ((c = scanner.read()) != ICharacterScanner.EOF && !Character.isWhitespace(c)) {
+				for (int i = 0; i < fDelimiters.length; i++) {
+					if (c == fDelimiters[i][0] && sequenceDetected(scanner, fDelimiters[i], true)) {
+						return fToken;
+					}
+				}
+			}
+			return fToken;
+		}
+		if (fDelimiters == null) {
+			fDelimiters = scanner.getLegalLineDelimiters();
+		}
+		int readCount = 1;
+		
+		// Find '](' and then find ')'
+		boolean sequenceFound = false;
+		int delimiterFound = 0;
+		while ((c = scanner.read()) != ICharacterScanner.EOF && delimiterFound < 2) {
+			readCount++;
+			if ( !sequenceFound && c == ']') {
+				c = scanner.read();
+				if (c == '(') {
+					readCount++;
+					sequenceFound = true;
+				} else {
+					scanner.unread();
+				}
+			} else if (c == ')') { // '](' is already found
+				return fToken;
+			}
+			
+			int i;
+			for (i = 0; i < fDelimiters.length; i++) {
+				if (c == fDelimiters[i][0] && sequenceDetected(scanner, fDelimiters[i], true)) {
+					delimiterFound ++;
+					break;
+				}	
+			}
+			if (i == fDelimiters.length)
+				delimiterFound = 0;
+		}
+		
+		for (; readCount > 0; readCount--)
+			scanner.unread();
+		return Token.UNDEFINED;
+	}
+	
+}
